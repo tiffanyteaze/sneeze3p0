47aa70608afc697410e3e6a47c46b543fa6696b5
Tiffany Teaze <tiffany.teaze@ruggable.com>
2021-08-08 16:31:07 -0700

    fixes errors

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js')
-const client = new Discord.Client()
+const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
 const config = require('./config.json')
 
 client.on('ready', () => {
